8110b3ba01fe3d4d80be339d0d484db0a56c8317
fattminer <me@matthew-finer.com>
2026-06-11 09:54:44 -0400

    updated logo

--- build.rs
+++ build.rs
@@ -5,5 +5,14 @@
 // by the Free Software Foundation.
 
 fn main() {
+    #[cfg(windows)]
+    {
+        let mut resource = winresource::WindowsResource::new();
+        resource.set_icon("assets/remouseable.ico");
+        resource
+            .compile()
+            .expect("failed to embed Windows application icon");
+    }
+
     slint_build::compile("ui/remouseable.slint").expect("failed to compile Slint UI");
 }
